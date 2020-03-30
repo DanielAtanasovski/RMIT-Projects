9dc523acbb58af13ebae4fce13f46fde02d6d1e4
James <s3656070@student.rmit.edu.au>
2020-03-30 15:30:43 +1100

    added explanation of plan
    added description and diagram of how i'm going to approach this problem

--- BinarySearchTreeRQ.java
+++ BinarySearchTreeRQ.java
@@ -26,29 +26,71 @@ public class BinarySearchTreeRQ implements Runqueue {
 	 * A better idea would be to balance only when needed. But I am unsure of how to do this...
 	 * 
 	 * 
+	 *
+	 * 				-- Example structure of Binary Search Tree using an array. --
+	 * -+---------------+---------------------------------------------------+------------------------------+-
+	 * 	| Depth/level:	|		Example Tree:		NOTE: ? = NULL value	| Index value: (base of 2)	   |
+	 * -+---------------+---------------------------------------------------+------------------------------+-
+	 * 	|	0			|			? - D									|	2							|
+	 * 	|				|			    /\									|								|	
+	 * 	|	1			|				B F									|	4							|
+	 *	|				|			   /\ /\								|								|
+	 * 	|	2			|			  A C D H    <-- Notice a duplicate D	|	8							|
+	 * 	|				|			 /\ |\|\ |\								|								|
+	 * 	|	3			|			? ? ??? ?G I							|	16							|
+	 * -+---------------+---------------------------------------------------+-------------------------------+
+	 * 
+	 * 								--	Tree represented in array.	--
+	 * 		-+----------+---+---+---+---+---+---+---+---+---+---+----+----+----+----+----+----+----+--+-+-
+	 * 		 |Index:	| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 |
+	 *  	+-----------+---+---+---+---+---+---+---+---+---+---+----+----+----+----+----+----+----+----+
+	 * 		 |Value:	| ? | D | B | F | A | C | D | H | ? | ? | ?  | ?  | ?  | ?  | ?  | ?  | G  | I  |
+	 * 		-+----------+---+---+---+---+---+---+---+---+---+---+----+----+----+----+----+----+----+----+-
+	 *						   
+	 * 						  ^
+	 *						  |-- The root of the whole tree.
 	 */
-	private Node[] arrayOfNodes;
+	
+	
+	
+	private Proc[] arrayOfNodes;
 
-	
-	
     /**
      * Constructs empty queue
      */
-	private static final int ROOT = 1; 
-	
+	private static final int TREE_ROOT_INDEX = 1; 
+	private static final int BASE_TREE_SIZE = 2;
     public BinarySearchTreeRQ() {
-    	//we start the array as a size of 2. Everything will be with a base of 2.
-    	//	Index 0 will be a null value to set up the array for the algorithm
-    	//	Index 1 will be the root of the tree.
-        arrayOfNodes = new Node[2];
+        arrayOfNodes = new Proc[BASE_TREE_SIZE];
 
     }  // end of BinarySearchTreeRQ()
 
 
     @Override
     public void enqueue(String procLabel, int vt) {
-        Node node = new Node(procLabel, vt);
-        
+        Proc nodeToAdd = new Proc(procLabel, vt);
+        //see if the tree doesnt even have a root set yet
+        if(arrayOfNodes.length == BASE_TREE_SIZE && arrayOfNodes[TREE_ROOT_INDEX] != null) {
+        	arrayOfNodes[TREE_ROOT_INDEX] = nodeToAdd;
+        }else {
+        	
+        	/*	NOTE: 
+        	 * 	- 	As seen in the Assignment 1 discussion, it doesn't matter if it is balanced....
+        	 *		I will try my best to have it balanced as it will speed up searching.
+        	 * 	-	The VT values are not unique and there for there is no way of making 
+        	 * 		a perfectly balanced tree with duplicates of values, thus i will do the following:
+        	 * 
+        	 * 		-	If the vt of the Proc is < the parent it will go on the LEFT of the parent.
+        	 * 		-	If the vt of the Proc is > OR = to the parent it will go on the RIGHT of the parent.
+        	 * 
+        	 * 	- (See tree diagram above about duplicates)
+        	 */
+        	
+        	
+        	//make new method
+
+        	if(nodeToAdd.getVt() < )
+        }
         
     } // end of enqueue()
 
@@ -98,5 +140,14 @@ public class BinarySearchTreeRQ implements Runqueue {
         // Implement me
 
     } // end of printAllProcess()
-
+    
+    
+    /////// Custom methods ///////
+    
+    private int getNumberOfLevels() {
+		return (int) (Math.log(arrayOfNodes.length) / Math.log(2));
+    }
+   
+	
+    
 } // end of class BinarySearchTreeRQ
