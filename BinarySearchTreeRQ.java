b5af3e5fddc11ccc051f86d8ed1c91ad9a076161
James <s3656070@student.rmit.edu.au>
2020-03-30 13:40:46 +1100

    Started the bst
    Started to plan and set up the object,

--- BinarySearchTreeRQ.java
+++ BinarySearchTreeRQ.java
@@ -11,19 +11,44 @@ import java.lang.String;
  */
 public class BinarySearchTreeRQ implements Runqueue {
 
+	
+	/*
+	 * BASIC IDEA:
+	 * - As this is a Binary tree all elements need to be balanced
+	 * as such every removal and addition of a node the tree needs to 
+	 * re-balance itself. My thinking is that we make a balance() method to handle it
+	 * 
+	 * - balance()
+	 * this will essentially make a copy of the whole tree and then one by one, 
+	 * feed each element into the original tree to then re-balance. However this isn't
+	 * that efficient as it will cause the whole tree to be 're made' every deletion or insertions
+	 * 
+	 * A better idea would be to balance only when needed. But I am unsure of how to do this...
+	 * 
+	 * 
+	 */
+	private Node[] arrayOfNodes;
+
+	
+	
     /**
      * Constructs empty queue
      */
+	private static final int ROOT = 1; 
+	
     public BinarySearchTreeRQ() {
-        // Implement Me
+    	//we start the array as 2, since the first index will always be null
+    	//the 
+        arrayOfNodes = new Node[2];
 
     }  // end of BinarySearchTreeRQ()
 
 
     @Override
     public void enqueue(String procLabel, int vt) {
-        // Implement me
-
+        Node node = new Node(procLabel, vt);
+        
+        
     } // end of enqueue()
 
 
