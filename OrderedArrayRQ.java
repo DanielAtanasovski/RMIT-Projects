f5ec7da587187ea9d505a50e72a9582d84a71ab3
Daniel Atanasovski <dtatanasovski@gmail.com>
2020-03-31 17:39:52 +1100

    Ordered Arrays
    + Enqueue
    + Deque
    + Sorted
    + Print all

--- OrderedArrayRQ.java
+++ OrderedArrayRQ.java
@@ -12,27 +12,87 @@ import java.lang.String;
  */
 public class OrderedArrayRQ implements Runqueue {
 
+    private Proc processes[];
+    private int procCount;
+
     /**
      * Constructs empty queue
      */
     public OrderedArrayRQ() {
-        // Implement Me
+        processes = new Proc[10];
+        procCount = 0;
 
     }  // end of OrderedArrayRQ()
 
 
     @Override
     public void enqueue(String procLabel, int vt) {
-        // Implement me
+        // O(n)
+        
+        // Reached array limit, increase Size
+        if (processes.length-1 == procCount){
+            Proc tempArr[] = new Proc[procCount+1];
+            System.arraycopy(processes, 0, tempArr, 0, processes.length);
+            processes = tempArr;
+        }
+
+        // Add the new process
+        processes[procCount] = new Proc(procLabel, vt);
+        procCount++;
+
+        // Resort the array
+        sort();
 
     } // end of enqueue()
+
+    /** 
+     * Resorts the array
+    */
+    private void sort() {
+        // O(n)
+        // Based on bubble sort
+        boolean loop = true;
+        // We only stop looping when no swap occurs
+        while (loop) {
+            boolean swapOccurred = false;
+            for (int i = 0; i < procCount; i++) {
+                // If we aren't the last element (as there is nothing after it)
+                if ((i + 1) != procCount) {
+                    if (processes[i].getVt() > processes[i + 1].getVt()){
+                        // Swap processes
+                        Proc temp = processes[i];
+                        processes[i] = processes[i + 1];
+                        processes[i + 1] = temp;
+                        swapOccurred = true;
+                    }
+                }
+            }
+            // If no swaps happened, we can assume that it is sorted
+            if (!swapOccurred)
+                loop = false;
+        }
+    }
 
 
     @Override
     public String dequeue() {
-        // Implement me
+        String retString = "";
 
-        return ""; // placeholder,modify this
+        // If there is no processes
+        // list is empty
+        if (procCount != 0){
+            retString = processes[0].getLabel();
+
+            // Move each element up in the list
+            for (int i = 1; i < procCount; i++) {
+                processes[i - 1] = processes[i];
+            }
+        }
+
+        // Reduce count
+        procCount--;
+
+        return retString; // placeholder,modify this
     } // end of dequeue()
 
 
@@ -70,7 +130,13 @@ public class OrderedArrayRQ implements Runqueue {
 
     @Override
     public void printAllProcesses(PrintWriter os) {
-        //Implement me
+        for (int i = 0; i < procCount; i++) {
+            os.print(processes[i].getLabel());
+
+            if (i != (procCount-1))
+                os.print(" ");
+        }
+        os.println();
 
     } // end of printAllProcesses()
 
