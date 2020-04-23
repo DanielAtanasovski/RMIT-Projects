1bdb8895608dae4473e33862c022b0e04e1a0508
Daniel Atanasovski <dtatanasovski@gmail.com>
2020-04-23 12:08:41 +1000

    Testing Suite

---
+++ src/tests/DiceTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import tests.interfaces.Test;
+
+/**
+ * Performs a series of tests centred around the dice classes
+ * @author Daniel Atanasovski
+ */
+public class DiceTest implements Test {
+    @Override
+    public boolean StartTest() {
+        return false;
+    }
+}
---
+++ src/tests/GameEngineTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import tests.interfaces.Test;
+
+/**
+ * Performs a series of tests centred around the GameEngine class
+ * @author Daniel Atanasovski
+ */
+public class GameEngineTest implements Test {
+
+    @Override
+    public boolean StartTest() {
+        return false;
+    }
+}
