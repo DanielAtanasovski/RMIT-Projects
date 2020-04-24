0aef0578fbd6124a1ad0597c55338c2f5ac0e5a4
Daniel Atanasovski <dtatanasovski@gmail.com>
2020-04-24 16:32:54 +1000

    Cleanup

--- src/client/MyTest1Client.java
+++ src/client/MyTest1Client.java
@@ -1,15 +1,10 @@
 package client;
 
-import model.GameEngineImpl;
-import model.SimplePlayer;
-import model.interfaces.GameEngine;
-import model.interfaces.Player;
 import tests.DiceTest;
 import tests.GameEngineTest;
 import tests.PlayerTest;
 import tests.interfaces.Test;
 import validate.Validator;
-import view.GameEngineCallbackImpl;
 
 /**
  * 
@@ -20,10 +15,6 @@ public class MyTest1Client
 {
    public static void main(String args[])
    {
-      // TODO: Full test coverage
-      // instantiate the GameEngine so we can make calls
-      final GameEngine gameEngine = new GameEngineImpl();
-
       // call method in Validator.jar to test *structural* correctness
       // just passing this does not mean it actually works .. you need to test yourself!
       // pass false if you want to show minimal logging (pass/fail) .. (i.e. ONLY once it passes)
