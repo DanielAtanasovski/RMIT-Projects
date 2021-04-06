acad08e5e113e43997341402d96367e78e5e2ea7
Daniel Atanasovski <dtatanasovski@gmail.com>
2021-04-06 15:24:33 +1000

    - Removed more magic numbers and added more customisation to shape points. - Cleaned up player. - Player now using vectors for positioning.

--- src/entity/Player.cpp
+++ src/entity/Player.cpp
@@ -20,18 +20,12 @@
 
 Player::Player(float x, float y)
 {
-	/*inputManager.subscribeToKeyPress('w', std::bind(&Player::onMove, this, std::placeholders::_1));
-	inputManager.subscribeToKeyPress('s', std::bind(&Player::onMove, this, std::placeholders::_1));
-	inputManager.subscribeToKeyPress('a', std::bind(&Player::onMove, this, std::placeholders::_1));
-	inputManager.subscribeToKeyPress('d', std::bind(&Player::onMove, this, std::placeholders::_1));*/
-	this->x = x;
-	this->y = y;
+	position = Vector2(x, y);
 }
 
 Player::Player()
 {
-	this->x = 0;
-	this->y = 0;
+	position = Vector2(0, 0);
 }
 
 Player::~Player()
@@ -42,33 +36,33 @@ void Player::draw()
 {
 	glPushMatrix();
 
-	// Apply Rotation
-	glTranslatef(x, y, 0.0f);
+	// Apply Rotation and position
+	glTranslatef(position.x, position.y, 0.0f);
 	glRotatef(rotation, 0.0f, 0.0f, -1.0f);
 
-	// Outline \\ 
+	// Outline Shape 
 	glColor3f(OUTLINE_COLOUR.x, OUTLINE_COLOUR.y, OUTLINE_COLOUR.z);
 	// Left Half
 	glBegin(GL_LINE_LOOP);
-	glVertex3f(-5, -10, 0);
-	glVertex3f(0, 5, 0);
-	glVertex3f(0, -5, 0);
+	glVertex3f(DRAW_LEFT_POINT.x, DRAW_LEFT_POINT.y, 0);
+	glVertex3f(DRAW_TOP_POINT.x, DRAW_TOP_POINT.y, 0);
+	glVertex3f(DRAW_MID_POINT.x, DRAW_MID_POINT.y, 0);
 	glEnd();
 
 	// Right haf
 	glBegin(GL_LINE_LOOP);
-	glVertex3f(5, -10, 0);
-	glVertex3f(0, 5, 0);
-	glVertex3f(0, -5, 0);
+	glVertex3f(DRAW_RIGHT_POINT.x, DRAW_RIGHT_POINT.y, 0);
+	glVertex3f(DRAW_TOP_POINT.x, DRAW_TOP_POINT.y, 0);
+	glVertex3f(DRAW_MID_POINT.x, DRAW_MID_POINT.y, 0);
 	glEnd();
 
-	// Fill
+	// Fill Shape
 	glColor3f(FILL_COLOUR.x, FILL_COLOUR.y, FILL_COLOUR.z);
 	glBegin(GL_TRIANGLE_STRIP);
-	glVertex3f(- 5, - 10, 0);
-	glVertex3f(0, 5, 0);
-	glVertex3f(0, - 5, 0);
-	glVertex3f( 5, - 10, 0);
+	glVertex3f(DRAW_LEFT_POINT.x, DRAW_LEFT_POINT.y, 0);
+	glVertex3f(DRAW_TOP_POINT.x, DRAW_TOP_POINT.y, 0);
+	glVertex3f(DRAW_MID_POINT.x, DRAW_MID_POINT.y, 0);
+	glVertex3f(DRAW_RIGHT_POINT.x, DRAW_RIGHT_POINT.y, 0);
 	glEnd();
 
 	glPopMatrix();
@@ -80,10 +74,10 @@ void Player::update() {
 }
 
 void Player::boundsCheck() {
-	if (this->rotation > 360)
-		this->rotation = 0;
-	else if (this->rotation < 0)
-		this->rotation = 360;
+	if (rotation > 360)
+		rotation = 0;
+	else if (rotation < 0)
+		rotation = 360;
 
 	if (velocity > MOVE_SPEED)
 		velocity = MOVE_SPEED;
@@ -93,17 +87,17 @@ void Player::boundsCheck() {
 
 void Player::move()
 {
-	// Move
-	this->velocity = inputVector.y * MOVE_SPEED;
+	// Velocity
+	velocity = inputVector.y * MOVE_SPEED;
 	// Rotate
-	this->rotation += inputVector.x * ROTATE_SPEED;
+	rotation += inputVector.x * ROTATE_SPEED;
 	boundsCheck();
 
 	// Update Position
-	this->x += sinf(this->rotation * (M_PI / 180)) * this->velocity;
-	this->y += cosf(this->rotation * (M_PI / 180)) * this->velocity;
-	//std::cout << "rot: " << this->rotation << std::endl;
+	position.x += sinf(rotation * (M_PI / 180)) * velocity;
+	position.y += cosf(rotation * (M_PI / 180)) * velocity;
 
+	// Request Redraw
 	glutPostRedisplay();
 }
 
@@ -125,5 +119,4 @@ void Player::getInput()
 	if (Input::onPressed(KEY_RIGHT)) {
 		inputVector.x += 1;
 	}
-	//std::cout << "x: " << inputVector.x << std::endl;
 }
--- src/entity/Player.h
+++ src/entity/Player.h
@@ -12,7 +12,7 @@ public:
 	void update();
 
 private:
-	float x, y = 0.0f;
+	Vector2 position = Vector2(0, 0);
 	float rotation = 45.0f;
 	void getInput();
 	void move();
@@ -22,14 +22,22 @@ private:
 
 	// Settings
 	// TODO: Move to Game options class
+	// Controls
 	const char KEY_UP = 'W';
 	const char KEY_DOWN = 'S';
 	const char SHOOT = ' ';
 	const char KEY_LEFT = 'A';
 	const char KEY_RIGHT = 'D';
+	// Stats
 	const float ROTATE_SPEED = 5.0f;
 	const float MOVE_SPEED = 2.0f;
+	// Colours
 	const Vector3 OUTLINE_COLOUR = Vector3(1.0f, 0.0f, 0.0f);
 	const Vector3 FILL_COLOUR = Vector3(1.0f, 1.0f, 1.0f);
+	// Drawing
+	const Vector2 DRAW_TOP_POINT = Vector2(0, 5);
+	const Vector2 DRAW_LEFT_POINT = Vector2(-5, -10);
+	const Vector2 DRAW_MID_POINT = Vector2(0, -5);
+	const Vector2 DRAW_RIGHT_POINT = Vector2(5, -10);
 };
 
