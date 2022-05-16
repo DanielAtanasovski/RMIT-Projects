80437b1b4b6414ed4332ebcab28abf9f9a1b0d8d
Daniel Atanasovski <s3658474@student.rmit.edu.au>
2022-05-16 18:15:00 +1000

    Initial file architecture

---
+++ src/BTree.java
@@ -0,0 +1,15 @@
+public class BTree {
+
+    public BTree() {
+
+    }
+
+    public void load(String heapFilePath) {}
+    public void create() {
+
+    }
+
+    private void readHeap(String heapFilePath) {}
+
+
+}
